d86e41c72f14de3487003ecf36ee6fc65970d5bb
Oleksandr Zarichnyi <alexandrzarichnyi@gmail.com>
2025-11-06 21:01:25 +0200

    moves checkWinner func to a separate file

--- src/hooks/useGameStore.jsx
+++ src/hooks/useGameStore.jsx
@@ -1,18 +1,6 @@
 import { create } from 'zustand'
 
-const checkWinner = (board) => {
-  const lines = [
-    [0,1,2],[3,4,5],[6,7,8],
-    [0,3,6],[1,4,7],[2,5,8],
-    [0,4,8],[2,4,6]
-  ];
-
-  for (let [a,b,c] of lines) {
-    if (board[a] && board[a] === board[b] && board[a] === board[c]) return board[a];
-  }
-
-  return board.includes(null) ? null : 'Draw';
-}
+import { checkWinner } from '../components/checkWinnerFunc'
 
 export const useGameStore = create((set, get) => ({
   board: Array(9).fill(null),
